1f137c5f31c9d4005bb3b7e54f1eb824d19cf012
CaProgram <christianaswell09@gmail.com>
2025-01-05 21:05:50 -0600

    Add files via upload

---
+++ main (copy).py
@@ -0,0 +1,17 @@
+print("Marvel Movie Character Creator")
+print("--")
+print("Please answer Y for Yes and N for No")
+print()
+likeWebs = input("Do you like 'hanging around'?: ")
+if likeWebs == "N":
+  print("Then you are not Spider-man")
+
+GravelVoice = input("Do you have a 'gravelly' voice?: ")
+if GravelVoice == "N":
+  print("Aww, then you're not Korg")
+
+feelMarvelous = input("Do you often feel 'Marvelous'?: ")
+if feelMarvelous == "Y":
+ print("Then you are Captain Marvel! Hi!")
+else:
+  print("You are not Captain Marvel")
